1f408cd35fca3a3a6b3939273812898244339499
lys590932-cmyk <lys590932@gmail.com>
2026-08-23 13:38:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    يخزّن ملفات التطبيق للعمل بدون إنترنت ولتثبيته كتطبيق
    + يدعم إشعارات الموبايل الفعلية (Web Notifications)
    =========================================================== */
-const CACHE = 'alsalasil-driver-v7';
+const CACHE = 'alsalasil-driver-v8';
 
 // ملفات هيكل التطبيق (App Shell)
 const SHELL = [
